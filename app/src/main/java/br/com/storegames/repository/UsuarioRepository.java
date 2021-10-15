7bb40ec4d826166b72672ba1d3dbcdcb4ec634e5
paulogrillo <paulodigitalart@gmail.com>
2021-10-15 14:36:52 -0300

    Updated JUnit

--- app/src/main/java/br/com/storegames/repository/UsuarioRepository.java
+++ app/src/main/java/br/com/storegames/repository/UsuarioRepository.java
@@ -1,10 +1,13 @@
 package br.com.storegames.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.storegames.models.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	Optional<Usuario> findByUsuario(String usuario);
+	List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+	Usuario findByNome(String nome);
 }
 
